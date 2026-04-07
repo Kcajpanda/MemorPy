a9980e141c08126dc38bd7c1d0818038d75165a8
Kcajpanda <47287629+Kcajpanda@users.noreply.github.com>
2026-04-06 20:24:33 -0700

    got it to handle puncuation i think, even things lik can't. puncuatiuon not at the end of a word after being split is missed though, work on more advanced process for identifying  aword

--- classes/Parser.py
+++ classes/Parser.py
@@ -1,6 +1,47 @@
 class Parser:
-
+    """
+    Currently just serves as a class to contain the parse method, and keep a history of all things parsed.
+    """
     def __init__(self):
-        pass
+        self.hist = []
+        self.hist_raw = []
 
+    def get(self, index):
+        """
+        Returns the parsed text found at self.hist(index)
+        """
+        return self.hist[index]
     
+    def get_raw(self, index):
+        """
+        Returns the raw text found at self.hist_raw(index)
+        """
+        return self.hist_raw[index]
+    
+    def parse(self, filename:str) -> list:
+        """
+        Opens file of given name in read mode, parses its content based on spaces, saves the raw and parse to their hist and thn returns the parsed.
+        """
+        with open(filename, "r") as file:
+            content = file.read()
+        self.hist_raw.append(content)
+
+        parsed_text = content.split()
+
+        tracked_chng = []
+        
+        for w in range(len(parsed_text)):
+            word = parsed_text[w]
+            if not word[-1].isalpha():
+                char = word[-1]
+                split_word = word.split(char)
+                parsed_text[w] = split_word[0]
+                tracked_chng.append([w+1, char])
+        
+        spacing = 0
+        for chng in tracked_chng:
+            parsed_text.insert(chng[0]+spacing, chng[1])
+            spacing+=1
+        
+        self.hist.append(parsed_text)
+        return parsed_text
--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 from classes.Text import Text
+from classes.Parser import Parser
+import copy
 
-parsed_without_punc = ["hi", ",", "lets", "learn", ":", "this", "string"]
+parse_obj = Parser()
+parsed_text = parse_obj.parse("./test_text/test_1.txt")
 
-text = Text(parsed_without_punc)
+text = Text(parsed_text)
 print(f"text.text = {text.text}")
-# print(f"text.blanked = {text.blanked}")
-# print(f"text.full_blanked = {text.full_blanked}")
+# # print(f"text.blanked = {text.blanked}")
+# # print(f"text.full_blanked = {text.full_blanked}")
 print(f"text.rand_lst = {text.rand_lst}")
 
-text.set_level(0)
-print(text.display_txt())
-text.set_level(3)
+# # text.set_level(0)
+# # print(text.display_txt())
+text.set_level(4)
 print(text.display_txt())
 
-# print(text.max_level)
-# print(text.display_txt())
+# # print(text.max_level)
+# # print(text.display_txt())
+
+# test = "hi, my name is jack."
+# split_list = test.split()
+# split_list_copy = copy.copy(split_list)
+# tracked_chng = []
+# print(split_list)
+
+# for w in range(len(split_list)):
+#     word = split_list[w]
+#     for c in range(len(word)):
+#         if not word[c].isalpha():
+#             char = word[c]
+#             # print(f"char: {char}")
+#             split_word = word.split(char)
+#             # print(f"split_word = {split_word}")
+#             split_list[w] = split_word[0]
+#             tracked_chng.append([w+1, char])
+
+# print(tracked_chng)
+# spacing = 0
+# for chng in tracked_chng:
+#     split_list.insert(chng[0]+spacing, chng[1])
+#     spacing+=1
+
+# print(split_list)
