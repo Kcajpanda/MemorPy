5c41d07508056104ba3d7b56261223f554f040e1
Kcajpanda <47287629+Kcajpanda@users.noreply.github.com>
2026-04-07 20:14:22 -0700

    Fixed docs and cleaned code

--- classes/Entry.py
+++ classes/Entry.py
@@ -1,13 +1,35 @@
 class Entry:
+    """
+    Abstraction of a single piece of text. Contains 
+    - self.val : The text used in creation.
+    - self.length : the length of self.val
+    
+    The class is designed to serve as a parent with children exercising a wider functionality. instances of Entry and all children can only be created by the static method Entry.validate, making it so you can trust that the class type of an entry to reflects what it contains (based on the rules set forth in validate) without having to examine the self.val further.
+    """
 
     def __init__(self, val:str) -> None:
+        """
+        __init__ for Entry parent class, see Entry descr:
+
+        Abstraction of a single piece of text. Contains 
+        - self.val : The text used in creation.
+        - self.length : the length of self.val
+        
+        The class is designed to serve as a parent with children exercising a wider functionality. instances of Entry and all children can only be created by the static method Entry.validate, making it so you can trust that the class type of an entry to reflects what it contains (based on the rules set forth in validate) without having to examine the self.val further.
+        """
         self.val = val
         self.length = len(val)
 
     def get_val(self) -> str:
+        """
+        Returns self.val.
+        """
         return self.val
     
     def get_len(self) -> int:
+        """
+        Returns self.length.
+        """
         return self.length
     
     def __str__(self) -> str:
@@ -21,15 +43,29 @@ class Entry:
     
     @staticmethod
     def validate(text):
+        """
+        Used for Creation of a instance type Entry or any of its children classes. The current rules are:
+        - single char alpha str are defined as Word objs (Case 1.1).
+            - EX: I, a, etc.
+        - single char non-alpha str are defined as Punc objs (Case 1.2).
+            - EX: ", -, etc.
+        - multi char all alpha str are defined as Word objs (Case 2.1).
+            - EX: Hello, wow, HI, etc.
+        - multi char mixed alpha / non-alpha str are defined as Word objs (Case 2.2.1).
+            - EX: I___, (Hello), W__wo, etc.
+        - multi char non-alpha str are defined as Punc objs (Case 2.2.1).
+            - EX: ____, (__*), ?:"{}}, etc.
+        - Anything else that mannages to slip through is made a Entry obj (Case 3.)
+        """
         if len(text) == 1:
-            # Word like: I, a, etc
+            # Case 1.1 Word like: I, a, etc
             if text.isalpha():
                 return Word(text)
-            #Must be punc
+            # Case 1.2 Must be punc
             else:
                 return Punc(text)
         elif len(text) > 1:
-            # Word like: Hi, Samuel, etc
+            # Case 2.1 Word like: Hi, Samuel, etc
             if text.isalpha():
                 return Word(text)
             # Word Like: H12, H_HU_H, K___
@@ -42,24 +78,41 @@ class Entry:
                         num_alpha += 1
                     else:
                         num_punc += 1
+                # Case 2.2.1
                 if num_alpha > 0:
                     return Word(text)
+                # Case 2.2.2
                 else:
                     return Punc(text) #TODO add more advanced classification later
         else:
-            # could cause lots of entries instead of words later
+            # Case 3. could cause lots of entries instead of words later
             return Entry(text)
     
 class Word (Entry):
+    """
+    A child of the Entry class. Serves as storage for the base string (self.val) but also has two specialized versions. Each instance has a pointer self.curr pointed at one of its text versions (val, blank, full) which can be swapped to other using self.state and its related methods. The pointer is used so that other classes only need interact with one aspect of the obj and then send commands to swap or update state as needed.
+    """
 
     def __init__(self, val:str) -> None:
+        """
+        __init__ for Word class, see class descr:
+
+        A child of the Entry class. Serves as storage for the base string (self.val) but also has two specialized versions. Each instance has a pointer self.curr pointed at one of its text versions (val, blank, full) which can be swapped to other using self.state and its related methods. The pointer is used so that other classes only need interact with one aspect of the obj and then send commands to swap or update state as needed. This style is maintained among all children of the Entry class so looping over a list of Entry objs is easy.
+        - self.blank : Contains a str of the same len as self.val but with all chars except the first replaced with "_".
+        - self.full_blank :  Contains a str of the same len as self.val but with all chars replaced with "_".
+        - self.state : A bool used to switch between which of the text alternates self.curr points to, see update_curr().
+        - self.curr : Pointer for whatever text alternate is currently set by self.state, see update_curr().
+        """
         super().__init__(val)
         self.blank, self.full_blank = "", ""
         self.state = True
         self.curr = self.val
-        self.det_blanks()
+        self._det_blanks()
 
-    def det_blanks(self) -> None:
+    def _det_blanks(self) -> None:
+        """
+        Method used by __init__ to set the alternate versions of self.val. Single char Words have an identical self.blank and self.full_blank. For multi-char words self.blank retains the first char but blanks "_" all other chars. self.full_blank fully blanks all chars.
+        """
         if self.get_len() == 1:
             self.blank, self.full_blank = "_", "_"
         else:
@@ -70,41 +123,46 @@ class Word (Entry):
             self.full_blank = "_" + tmp_str
 
     def get_val(self) -> str:
+        """
+        Uses parent method to return it's self.val. 
+        """
         return super().get_val()
     
     def get_len(self):
+        """
+        Uses parent method to return it's self.length. 
+        """
         return super().get_len()
     
-    def get_blank(self):
-        return self.blank
+    def update_curr(self) -> None:
+        """
+        Checks curr value of state as updates self.curr to point at the corresponding var.
+        """
+        if self.state:
+            self.curr = self.val
+        else:
+            self.curr = self.blank
     
-    # def update_curr(self) -> None:
-    #     if self.state:
-    #         self.curr = self.val
-    #     else:
-    #         self.curr = self.blank
-    
-    # def swap_state(self) -> None:
-    #     self.state = not self.state
-    #     # self.update_curr()
+    def swap_state(self) -> None:
+        """
+        Inverts value of the bool self.state then calls an update via self.update_curr() to update self.curr
+        """
+        self.state = not self.state
+        self.update_curr()
 
-    # def set_state(self, state:bool) -> None:
-    #     self.state = state
-        # self.update_curr()
+    def set_state(self, state:bool) -> None:
+        """
+        Directly sets the value of the bool self.state then calls an update via self.update_curr() to update self.curr
+        """
+        self.state = state
+        self.update_curr()
 
-    # def get_curr(self):
-    #     self.update_curr()
-    #     if self.state:
-    #         self.curr = self.val
-    #     else:
-    #         self.curr = self.blank
-    #     return self.curr
-    
-    def get_blank(self) -> str:
-        return self.blank
-    
-    def get_full_blank(self) -> str:
-        return self.full_blank
+    def get_curr(self):
+        """
+        My call of other objs for this class, forces update of self.state to ensure self.curr is pointed right then returns self.curr.
+        """
+        self.update_curr()
+        return self.curr
     
     def __str__(self):
         return self.get_curr()
@@ -113,24 +171,38 @@ class Word (Entry):
         return super().__eq__()
     
 class Punc (Entry):
+    """
+    A child of the Entry class. Serves meant to contain data an metadata similar to Word, but since punctuation doesn't need alternates like a Word obj does most of methods that mirror a Word just return self.val. hence why this class doesn't have a self.state.
+    """
 
     def __init__(self, val:str) -> None:
+        """
+        __init__ for Punc class. See class descr:
+
+        A child of the Entry class. Mant to contain data an metadata similar to Word, but since punctuation doesn't need alternates like a Word obj does most of methods that mirror a Word just return self.val. It mirrors the Word class only in ways that make looping over a list of Entry child objects easy. hence why this class doesn't have a self.state.
+        """
         super().__init__(val)
     
     def get_val(self) -> str:
+        """
+        Uses parent method to return it's self.val. 
+        """
         return super().get_val()
     
-    def get_blank(self) -> str:
-        return super().get_val()
-    
-    # def get_curr(self) -> str:
-    #     return self.get_val()
+    def get_curr(self) -> str:
+        """
+        A mirror of the Word class's get_curr(). Since Punc instances don't have state or alt text versions it just return self.val.
+        """
+        return self.get_val()
     
     def get_len(self):
+        """
+        Uses parent method to return it's self.length. 
+        """
         return super().get_len()
     
     def __str__(self):
-        return self.get_curr()
+        return self.get_val()
     
     def __eq__(self):
         return super().__eq__()
--- classes/Text.py
+++ classes/Text.py
@@ -6,37 +6,58 @@ class Text:
     """
     Serves as an object Containing the relevant data and metadata for the memorization UI.
 
-    - self.text : List of full parsed text.
-    - self.blanked : Contains every entry from self.text but replaces every letter, except the first letter, in each word with '_'. Punctuation is added unaltered.
-        - EX: self.text[0] = "Word", self.blanked[0] = "W___"
-    - self.full_blank : Contains every entry from self.text but replaces every letter in each word with '_'. Punctuation is added unaltered.
-        - EX: 
-                self.text[0] = "Word", self.blanked[0] = "____"
-    - self.rand_lst : Contains a list of numbers, with each number referencing an index in self.text of a non-punctuation entry; its used with level to tell which indexes of self.displayed should be replaced with blanked text.
-    - self.level : Used to tell how many elements of self.rand_lst to reference.
+    - self.text : List of parsed text, stored as Entry instances (or a child class of Entry), given by the Parser class. This list is never mutated, but  has its Entry obj instances and their properties accessed by lists like self.displayed.
+    - self.text_len : length of self.text.
+    - self.rand_lst : Contains a list of numbers, with each number referencing an index of a Punc obj instance in self.text. The list stores the indexes ins a random order. Its used with self.level to tell which indexes of self.displayed should be replaced with blanked text.
+    - self.level : Used to tell how many elements of self.rand_lst to reference. The first self.level (excluding the first) number of indexes are grabbed to tell self.displayed which indexes to update with blanked or unblanked text. See self.update().
         - EX: 
                 self.text = ['hi', ',', 'lets', 'learn', ':', 'this', 'string']
                 self.rand_lst = [5, 6, 3, 2, 0]
 
                 # if level is 0
-                self.display_txt() => ['hi', ',', 'lets', 'learn', ':', 'this', 'string']
+                self.out_txt(0) => ['hi', ',', 'lets', 'learn', ':', 'this', 'string']
 
                 # if level is 3
-                self.display_txt() => ['hi', ',', 'lets', 'l____', ':', 't___', 's_____']
+                self.out_txt(0) => ['hi', ',', 'lets', 'l____', ':', 't___', 's_____']
         
-    - self.max_level : The total number of non punctuation entries in self.text.        
-    - self.displayed : Var that is used to display the text thats printed, initially its equal to self.text, but base don self.level and self.rand_lst is mutated to show the final text.
+    - self.max_level : The total number of Word obj instances in self.text.        
+    - self.displayed : List used to store each .get_curr() of each Entry obj in self.text. Relies on self.rand_lst and self.level to know which indexes to request and updated .get_curr() from.
         - EX: 
                 self.text = ['hi', ',', 'lets', 'learn', ':', 'this', 'string']
                 self.rand_lst = [3, 2, 0, 5, 6]
 
                 # if level is 3
-                self.display_txt() => ['h_', ',', 'l___', 'l____', ':', 'this', 'string']
+                self.out_txt(0) => ['h_', ',', 'l___', 'l____', ':', 'this', 'string']
     """
 
     def __init__(self, valid_txt:list) -> None:
         """
-        Creates a Text object, see __init__() comments or class descr for more details.
+        Creates a Text object, see class descr:
+
+        Serves as an object Containing the relevant data and metadata for the memorization UI.
+
+        - self.text : List of parsed text, stored as Entry instances (or a child class of Entry), given by the Parser class. This list is never mutated, but  has its Entry obj instances and their properties accessed by lists like self.displayed.
+        - self.text_len : length of self.text.
+        - self.rand_lst : Contains a list of numbers, with each number referencing an index of a Punc obj instance in self.text. The list stores the indexes ins a random order. Its used with self.level to tell which indexes of self.displayed should be replaced with blanked text.
+        - self.level : Used to tell how many elements of self.rand_lst to reference. The first self.level (excluding the first) number of indexes are grabbed to tell self.displayed which indexes to update with blanked or unblanked text. See self.update().
+            - EX: 
+                    self.text = ['hi', ',', 'lets', 'learn', ':', 'this', 'string']
+                    self.rand_lst = [5, 6, 3, 2, 0]
+
+                    # if level is 0
+                    self.out_txt(0) => ['hi', ',', 'lets', 'learn', ':', 'this', 'string']
+
+                    # if level is 3
+                    self.out_txt(0) => ['hi', ',', 'lets', 'l____', ':', 't___', 's_____']
+            
+        - self.max_level : The total number of Word obj instances in self.text.        
+        - self.displayed : List used to store each .get_curr() of each Entry obj in self.text. Relies on self.rand_lst and self.level to know which indexes to request and updated .get_curr() from.
+            - EX: 
+                    self.text = ['hi', ',', 'lets', 'learn', ':', 'this', 'string']
+                    self.rand_lst = [3, 2, 0, 5, 6]
+
+                    # if level is 3
+                    self.out_txt(0) => ['h_', ',', 'l___', 'l____', ':', 'this', 'string']
         """
         #TODO add ability to use blanked and full blanked
 
@@ -63,67 +84,7 @@ class Text:
         self.rand_lst.insert(0, None)
 
         for entry in self.text:
-            self.displayed.append(entry.get_val())
-
-        # for debug
-        # debug = True
-        
-        # if debug:
-        #     print(f"{self.text}")
-        #     print(f"Contains {self.text_len} entries")
-        #     print(f"{self.max_level} valid words detected, max level set")
-        # end debug
-        
-        # # Generates two lists, both have the same numbers of words as the original text except full_blank has every char replaced with '_' where blanked has all chars except the first replaced with '_'. So that for the el "word", full_blank would have it as "____" and blanked as "w___"
-        # # for word in self.text:
-
-        # #     if word.isalpha():
-        # #         tmp_str = ""
-        # #         # Normal length word
-        # #         if len(word) > 1:
-        # #             for i in range(len(word)-1):
-        # #                 tmp_str += "_"
-        # #             self.blanked.append(word[0] + tmp_str)
-        # #             self.full_blank.append(tmp_str + "_")
-        # #         # Single char word. EX I, a, etc
-        # #         elif len(word) == 1:
-        # #             self.blanked.append("_")
-        # #             self.full_blank.append("_")
-        # #         # Shouldn't be possible at this point in execution, but error raised just in case
-        # #         else:
-        # #             raise InvalidWordLength(word)
-        # #     else:
-        # #         # to keep index continuity with self.text we add the punctuation unaltered, even though we never call it
-        # #         self.blanked.append(word)
-        # #         self.full_blank.append(word)
-        # # for word in self.text:
-        # #     tmp_str = ""
-        # #     if self.is_word(word):
-        # #         if len(word) > 1:
-        # #             for i in range(len(word)-1):
-        # #                 tmp_str += "_"
-        # #             self.blanked.append(word[0] + tmp_str)
-        # #             self.full_blank.append(tmp_str + "_")
-        # #         elif len(word) == 1:
-        # #             self.blanked.append("_")
-        # #             self.full_blank.append("_")
-        # #         else:
-        # #             raise InvalidWordLength(word)
-        # #     else:
-        # #         self.blanked.append(word)
-        # #         self.full_blank.append(word)
-
-        # # Generates a list of ascending and ordered numbers called tmp_text_nums for as many words as there are in self.text, then loops for as many words as there grabbing a random number each time, adding it to self.rand_nums and then popping it from tmp_text_nums
-       
-
-        # for debug
-        # if debug:
-        #     print(f"order of removal: {self.rand_lst}")
-        #     self.indexes = {}
-        #     for nums in range(self.text_len):
-        #         self.indexes.update({nums: self.text[nums].get_val()})
-        #     print(f"self.indexes: {self.indexes}")
-        # end debug
+            self.displayed.append(entry.get_curr())
 
     # Getters
     def get_level(self) -> int:
@@ -147,24 +108,20 @@ class Text:
     # Level Methods
     def inc_level(self, num:int=1) -> None:
         """
-        Adds given num to self.level as long as it keep its in range.
-
-        EX:
+        Adds given num to self.level as long as it keeps it less than or equal to self.max_level.
         """
         # print(f"level: {self.level} + {num} <= self.max_level: {self.max_level} => {self.level + num <= self.max_level}")
         if self.level + num <= self.max_level:
             self.level += num
-        if self.level == self.max_level: print("Max Level")
+        # if self.level == self.max_level: print("Max Level")
 
     def dec_level(self, num:int=1) -> None:
         """
-        Subtracts given num to self.level as long as it keep its in range.
-
-        EX:
+        Subtracts given num to self.level as long as it keep its greater than or equal to 0.
         """
         if self.level - num >= 0 :
             self.level -= num
-        if self.level == 0: print("Easiest level")
+        # if self.level == 0: print("Easiest level")
 
     def set_level(self, num:int=0) -> None:
         """
@@ -178,32 +135,13 @@ class Text:
         else:
             raise OutOfLevelError(num)
         
-    #TODO add an update method so it just re-scans instead of recreating display each time
-        
     # Text Methods
-
-    def out_txt(self, prev_level) -> str:
+    def out_txt(self, prev_level:int=0) -> str:
         """
-        Gens display text and then goes through looking for non alpha chars, but not blanked words (words with '_" at the end), and readds appropriate spacing.
+        Forces self.displayed to update self, then goes through looking for non alpha chars, but not blanked words (words with '_" at the end), and re-adds appropriate spacing. In essence it un-parses the text but this time includes the changes made to blank out the text.
         """
-        # self.gen_display_txt()
         pretty_out = ""
-        # for i in range(len(self.displayed)):
-        #     # Avoids out of bounds
-        #     if i+1 != len(self.displayed):
-        #         # Is current word a punc mark?
-        #         curr_item = self.displayed[i]
-        #         next_item = self.displayed[i+1]
-        #         if curr_item[-1].isalpha() or curr_item[-1].__contains__("_"):
-        #             # Is the second word a punc mark?
-        #             if next_item[-1].isalpha() or next_item.__contains__("_"):
-        #                 pretty_out += f"{self.displayed[i]} "
-        #             else:
-        #                 pretty_out += f"{self.displayed[i]}"
-        #         else:
-        #             pretty_out += f"{self.displayed[i]} "
-        #     else:
-        #         pretty_out += self.displayed[i]
+
         self.update(prev_level)
         for i in range(len(self.displayed)):
             # Avoids out of bounds
@@ -222,29 +160,11 @@ class Text:
             else:
                 pretty_out += self.displayed[i]
         return pretty_out
-
-    def gen_display_txt(self) -> str:
-        """
-        Based on level, display the text but replaces the first [level] words in self.rand_lst with the corresponding word from self.blanked
-        """
-        self.reset_displayed()
-
-        if self.level != 0:
-            for num in range(1, self.level+1):
-                index = self.rand_lst[num]
-
-        print(f"len(self.rand_lst) = {len(self.rand_lst)}")
-        print(f"Pre-add text level : {self.level}, self.level: {self.level} > 0 => {self.level > 0}")
-        if self.level > 0:
-            for num in range(1, self.level+1):
-                print(f"num: {num}")
-                index = self.rand_lst[num]
-                print(f"self.rand_lst[num] / index to remove = {self.rand_lst[num]}")
-                self.displayed[index] = self.blanked[index]
-
-        return self.displayed
     
     def update(self, prev_level: int) -> None:
+        """
+        Compares the level of the previous out_txt (passed as an arg), and the current level request (via a live self.level call), to determine which indexes of self.displayed need to make update what they point to via .get_curr(). Compares the curr and prev level to understand which direction to iterate in.
+        """
         diff = self.level - prev_level
         # print(f"diff = {diff}")
         if diff > 0:
@@ -253,14 +173,18 @@ class Text:
                 # index = self.rand_lst[num]
                 # print(f"index to change: {index}, corresponds to => {self.text[index]}")
                 # print(f"num: {num}, corresponds to {self.text[num].get_blank()}")
-                self.displayed[num] = self.text[num].get_blank()
+                # self.displayed[num] = self.text[num].get_curr()
+                self.text[num].swap_state()
+                self.displayed[num] = self.text[num].get_curr()
         else:
             # print(f"Working in range: {self.rand_lst[self.level+1: prev_level+1]}")
             for num in self.rand_lst[self.level+1: prev_level+1]:
                 # index = self.rand_lst[num]
                 # print(f"index to change: {index}, corresponds to => {self.text[index]}")
                 # print(f"num: {num}, corresponds to {self.text[num].get_val()}")
-                self.displayed[num] = self.text[num].get_val()
+                # self.displayed[num] = self.text[num].get_curr()
+                self.text[num].swap_state()
+                self.displayed[num] = self.text[num].get_curr()
         
 
 class OutOfLevelError (Exception):
@@ -271,14 +195,4 @@ class OutOfLevelError (Exception):
 
     """
     def __init__(self, input, message="Error level negative, GIVEN="):
-        super().__init__(f"{message}{input}")
-
-class InvalidWordLength (Exception):
-    """
-    Exception for when a entry in self.text has len of 0.
-
-    EX:
-
-    """
-    def __init__(self, input, message="Error entry cannot be of length 0, GIVEN="):
         super().__init__(f"{message}{input}")
