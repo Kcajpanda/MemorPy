62e8fa841105f9e1391c2a1d83d706815423da95
Kcajpanda <47287629+Kcajpanda@users.noreply.github.com>
2026-04-07 20:47:44 -0700

    Chnaged memorpy to beging to reflect web style interaction and saving prev levels.

--- classes/MemorPy.py
+++ classes/MemorPy.py
@@ -6,6 +6,7 @@ class MemorPy:
     def __init__(self):
         self.parse = Parser()
         self.texts = []
+        self.level_hist = [0]
         self.prev_level = 0
         self.current_txt = None
 
@@ -23,18 +24,62 @@ class MemorPy:
     def set_curr(self, index):
         self.current_txt = self.texts[index]
 
+    def in_tokenizer(self, user_in:str) -> list:
+        tokens = user_in.split()
+        return tokens
+
     def interpret(self) -> None:
-        user_in = input("What's Next? (Harder = m or ENTER, Easier = b):")
-        self.prev_level = self.current_txt.get_level() 
-        if user_in == "m" or user_in == "M" or user_in == "":
-            self.current_txt.inc_level()
-            print(f"Text: {self.current_txt.out_txt(self.prev_level)}")
-            print(f"Level: {self.current_txt.level}")
-        elif user_in == "b" or user_in == "B":
-            self.current_txt.dec_level()
-            print(f"Text: {self.current_txt.out_txt(self.prev_level)}")
-            print(f"Level: {self.current_txt.level}")
-        elif user_in == "exit":
-            exit()
+        raw_user_in = input("What's Next? (Harder = m or ENTER, Easier = b):")
+        user_in = self.in_tokenizer(raw_user_in)
+        self.prev_level = self.current_txt.get_level()
+        self.level_hist.append(self.prev_level)
+
+        # Command logic
+        if len(user_in) == 1: 
+            if user_in[0] == "m" or user_in[0] == "M" or user_in[0] == "1":
+                self.c_inc_level()
+            elif user_in[0] == "b" or user_in[0] == "B" or user_in[0] == "2":
+                self.c_dec_level()
+            elif user_in[0] == "exit" or user_in[0] == "0":
+                self.memorpy_exit()
+            else:
+                print(f"Input not recognized: {user_in}, try again:")
+        elif len(user_in) == 2:
+            if user_in[0] == "set":
+                try:
+                    val = int(user_in[1])
+                except ValueError:
+                    raise InvalidCommandArg(user_in[1])
+                self.c_set_level(val)
+            elif user_in[0] == "upload":
+                pass
+                # take third arg in as text save to file of name provided in second arg
+            else:
+                print(f"Input not recognized: {user_in}, try again:")
         else:
-            print(f"Input not recognized: {user_in}, try again:")
+                print(f"Input not recognized: {user_in}, try again:")
+
+    def c_inc_level(self):
+        self.current_txt.inc_level()
+        print(f"Text: {self.current_txt.out_txt(self.prev_level)}")
+
+    def c_dec_level(self):
+        self.current_txt.dec_level()
+        print(f"Text: {self.current_txt.out_txt(self.prev_level)}")
+
+    def c_set_level(self, user_in):
+        self.current_txt.set_level(user_in)
+        print(f"Text: {self.current_txt.out_txt(self.prev_level)}")
+
+    def memorpy_exit(self):
+        exit()
+
+class InvalidCommandArg (Exception):
+    """
+    Exception for a command is arg is of the wrong type
+
+    EX:
+
+    """
+    def __init__(self, input, message="Error expected arg of type int for second arg, GIVEN="):
+        super().__init__(f"{message}{input}")
